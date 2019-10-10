6902ae3608e7ac7e67642ca3cc697fa922102d28
varshaprasad96 <vnarsing@andrew.cmu.edu>
2019-10-10 14:14:48 -0400

    [wip] simplify caliper - trying to run jar
    The current scala code is unable to connect to Azure DL and
    fetch the data, but works well on notebooks.

--- build.sbt
+++ build.sbt
@@ -4,9 +4,18 @@ version := "0.1"
 
 scalaVersion := "2.11.12"
 
+
 libraryDependencies ++= Seq(
   "org.apache.spark" %% "spark-core" % "2.3.2",
   "org.apache.spark" %% "spark-sql" % "2.3.2",
   "com.fasterxml.jackson.core" % "jackson-databind" % "2.2.2",
-  "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.2.2"
+  "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.2.2",
+  "mysql" % "mysql-connector-java" % "8.0.17",
+  // https://mvnrepository.com/artifact/com.microsoft.azure/azure-data-lake-store-sdk
+  "com.microsoft.azure" % "azure-data-lake-store-sdk" % "2.0.11"
 )
+
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
--- src/main/scala/SimplifyCaliper.scala
+++ src/main/scala/SimplifyCaliper.scala
@@ -1,15 +1,14 @@
+import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
+import org.apache.spark.SparkContext
 import org.apache.spark.sql.SparkSession
 
-import com.fasterxml.jackson.databind.{DeserializationFeature, ObjectMapper}
-import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
-import com.fasterxml.jackson.module.scala.DefaultScalaModule
-import org.apache.spark.sql.functions.input_file_name
 
 object MarshallableImplicits {   // From https://www.programcreek.com/scala/com.fasterxml.jackson.module.scala.DefaultScalaModule
 
   implicit class Unmarshallable(unMarshallMe: String) {
     def toMap: Map[String,Any] = JsonUtil.toMap(unMarshallMe)
-    //def toMapOf[V]()(implicit m: Manifest[V]): Map[String,V] = JsonUtil.toMapOf[V](unMarshallMe)
     def fromJson[T]()(implicit m: Manifest[T]): T =  JsonUtil.fromJson[T](unMarshallMe)
   }
 
@@ -40,6 +39,8 @@ object JsonUtil {
   }
 }
 
+
+
 case class Event(id: String, actor: String, event_type: String, action: String,
   event_object: String, event_time: String, target: String,
   generated: String, edApp: String, referrer: String,
@@ -48,20 +49,26 @@ case class Event(id: String, actor: String, event_type: String, action: String,
 
 object SimplifyCaliper extends App {
 
-  val spark = SparkSession.builder
-    .master("local[*]")
-    .appName("Spark Word Count")
-    .getOrCreate()
+//  val spark = SparkSession.builder
+//    .master("local[*]")
+//    .appName("Spark Word Count")
+//    .getOrCreate()
 
+  // Modifying to get sc from databricks
+  val sparkContext = SparkContext.getOrCreate()
+  val spark = SparkSession.builder().getOrCreate()
+
+  //  spark.sparkContext.hadoopConfiguration.set("fs.azure.account.auth.type.acctname.dfs.core.windows.net", "OAuth")
+  //  spark.sparkContext.hadoopConfiguration.set("fs.azure.account.oauth.provider.type.acctname.dfs.core.windows.net",  "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
+  //  spark.sparkContext.hadoopConfiguration.set("fs.azure.account.oauth2.client.id.acctname.dfs.core.windows.net", "<client-id>")
+  //  spark.sparkContext.hadoopConfiguration.set("fs.azure.account.oauth2.client.secret.acctname.dfs.core.windows.net", dbutils.secrets.get(scope = "<scope-name>", key = "<key-name-for-service-credential-for-service-credential>"))
+  //  spark.sparkContext.hadoopConfiguration.set("fs.azure.account.oauth2.client.endpoint.acctname.dfs.core.windows.net", "https://login.microsoftonline.com/<directory-id>/oauth2/token")
+
+
+  //
   val sc = spark.sparkContext
+ val rawCaliper = spark.sparkContext.textFile("abfss://data@saildatalake.dfs.core.windows.net/testcourse/f16-15319/raw/caliper/")
 
-  //val rawCaliper = spark.read.text("/Users/cbogart/dropbox/research/sail/spark/caliper/*")
-    //.select(input_file_name, $"value")
-    //.as[(String, String)] // Optionally convert to Dataset
-    //.rdd
-
-
-  val rawCaliper = sc.textFile("/Users/cbogart/dropbox/research/sail/spark/caliper/*")
 
 
   def object2id(obj : Object): String = obj match {
@@ -93,22 +100,55 @@ object SimplifyCaliper extends App {
       records.map( rec => rec + ("sensor" -> theMap("sensor").toString))
   }
 
-  val test1 = """{"id": 42, "name": "item42", "subthing": {"id": "42b", "subname": "subthing42"}}"""
+  val test1 = """{"id": 42, "name": "item42", "subthing": {"id": "42b", "subname": "subthing42"}} """
   println(flatten(JsonUtil.toMap[Object](test1)))
 
-  val result = rawCaliper.zip(input_file_name()).mapPartitions(records => {
+  import spark.implicits._
+
+  val result = rawCaliper.mapPartitions(records => {
     records.map(JsonUtil.toMap[Object]).flatMap(
       decorate_and_split_caliper
     )
     }, true)
 
+  print(rawCaliper.getClass.getSimpleName())
+
   val events = result.mapPartitions(records => {
-    records.filter( q => (q contains "type") && q("type") == "Person"  )
+    records.filter( q => (q contains "type") && q("type") == "Person")
   })
 
-  println( "Ready!");
 
-  events.foreach(println)
+  val columns=events.take(1).flatMap(a=>a.keys)
+
+  val resultantDF=events.map{value=>
+    val list=value.values.toList
+    (list(0),list(1), list(2), list(3))
+  }.toDF(columns:_*)
+
+  resultantDF.printSchema()
+  resultantDF.show(2, false)
+
+  //create properties object
+
+//  val prop = new java.util.Properties
+//  prop.setProperty("driver", "com.mysql.jdbc.Driver")
+//  prop.setProperty("user", "XXXXXXXXXXX")
+//  prop.setProperty("user", "XXXXX")
+//  prop.setProperty("password", "XXXXXXXX")
+
+  //jdbc mysql url - destination database is named "fall2019" - local, "test" - remote
+//  val url = "jdbc:mysql://localhost:3306/fall2019"
+//  val url = "jdbc:mysql://localhost:3306/fall2019?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC"
+//  val url = "jdbc:mysql://XXXXXXXXXXXX/test?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC"
+
+  //destination database table
+//  val table = "localtransfer"
+
+  //write data from spark dataframe to database
+//  resultantDF.write.mode("append").jdbc(url, table, prop)
+  println( "Ready!");
+  //result.foreach(println);
+
 
   println("Done!");
 }
